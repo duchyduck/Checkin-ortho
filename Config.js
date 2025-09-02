ee39ee93fb25ccf5ca80f6d32bce7ec247f786d2
DUCHY <nichakan.ke.budc@cpird.in.th>
2025-09-02 13:31:19 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbycYAnTTkDa8mD2SgmiUvtOUwchGU7MNPYgiZ9YOLWwjBXTFpdSTMETu2TZCjVjj7-3/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbweRUbEmEy7s-7CVI3dgZz7wHgEPY7C-5VnhpNr3hFDWM0G3AzVVdQlaRuE4wiTtnZ0GQ/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbzI8vdZOgMop4DvTjkVUcRbme7ioty0ovlB670XgHy3YeTWrHUWygFUVrPh2U6m7sJe/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz-fl7LnQ_qwu6hP__Mfc7WuXA1toAln1Z-DKzyWYLb72bWCu048S4B5nJQmw4XTVBd/exec';
 //LIFF_ID
     const LIFF_ID = '2008034087-NkJYKX2M'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008034087-961xplQ3'; // LIFF ID history
