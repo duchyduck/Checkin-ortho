1810f1cfb40277fd81ac34c799d405c7b73e021b
DUCHY <nichakan.ke.budc@cpird.in.th>
2025-09-02 11:48:02 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,6 +2,6 @@
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbycYAnTTkDa8mD2SgmiUvtOUwchGU7MNPYgiZ9YOLWwjBXTFpdSTMETu2TZCjVjj7-3/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbweRUbEmEy7s-7CVI3dgZz7wHgEPY7C-5VnhpNr3hFDWM0G3AzVVdQlaRuE4wiTtnZ0GQ/exec';
 //LIFF_ID
-    const LIFF_ID = '2007475303-d5v41rEq'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2007475303-JYyrOdWn'; // LIFF ID history
-    const LIFF_REGISTER = '2007475303-7wJz410Q'; // LIFF ID register
+    const LIFF_ID = '2008034087-NkJYKX2M'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2008034087-961xplQ3'; // LIFF ID history
+    const LIFF_REGISTER = '2008034087-LxMXq23K'; // LIFF ID register
